986c0141b6351c14557465759e4c8413ff0998d6
Sakshi Hudge <sakshi.bhudge@gmail.com>
2025-06-18 17:17:24 +0530

    Update: made changes in LearnSphere backend

--- Backend/Root/index.js
+++ Backend/Root/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { connectDB } = require('./config/db');
+const connectDB = require('../config/db');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.use(express.json());
+
+// ✅ Add this route
+app.get('/', (req, res) => {
+  res.send('API is working!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
