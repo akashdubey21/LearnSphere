c769ae07ea610518e6fb84287c9ad4ef077d398f
Sakshi Hudge <sakshi.bhudge@gmail.com>
2025-06-18 11:10:01 +0530

    Your message about what you changed

---
+++ Backend/Root/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { connectDB } = require('./config/db');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
